aa391bc659a7ab9978622845ddfad11cbe4e6324
Ashish Sharma <grandmastrgogo@gmail.com>
2023-12-11 09:19:25 +0000

    fixed spelling mistakes in the hero content

--- components/hero.js
+++ components/hero.js
@@ -11,14 +11,17 @@ const Hero = () => {
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
               Hi, I am Psychologist Swapna
             </h1>
-            <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              I am a certified Ayurvedic daughter & a single mother. I am gifted with ability to sense the aura/energy body of the people I meet, 
+            <p className="py-5 text-l leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
+              I am a certified Ayurvedic doctor & a single mother. I am gifted with ability to sense the aura/energy body of the people I meet, 
               which enables me to see through their psychological problems, even if they are unable to articulate it. 
+            </p>
+            <p className="py-5 text-l leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
+              If you are facing issues for which you need counselling help, feel free to get in touch. I am running counselling sessions for free at the moment. 
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
-                href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
+                href="https://wa.me/918801543815"
                 target="_blank"
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-green-400 rounded-md ">
